5747fc6c9fb6a0fea402c29ab4074f10d2f73df6
Brianna DelValle <briannadelvalle@gmail.com>
2019-11-05 18:03:32 -0700

    Write snapshot test for Header

--- tests/unit/Header.spec.js
+++ tests/unit/Header.spec.js
@@ -8,4 +8,10 @@ describe('Header', () => {
   
     expect(wrapper.find('h1').text()).toEqual('Thesaurus')
   })
+
+  it('matches the snapshot', () => {
+    const wrapper = shallowMount(Header)
+
+    expect(wrapper).toMatchSnapshot();
+  })
 })
