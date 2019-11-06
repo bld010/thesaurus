8547c2436f0f1222bfb9ad7691ba6a65052dd793
Brianna DelValle <briannadelvalle@gmail.com>
2019-11-06 09:41:12 -0700

    Write status not ok error test in fetchSynonyms

--- tests/unit/apiCalls.spec.js
+++ tests/unit/apiCalls.spec.js
@@ -5,6 +5,8 @@ describe('apiCalls', () => {
     it('should fire fetch with the correct url', async () => {
       let expectedUrl = `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/happy?key=${process.env.VUE_APP_API_KEY}`
 
+      let mockResults = [['results'], ['more results']]
+
       let mockFetch = jest.fn().mockImplementation(() => {
         return Promise.resolve({
           ok: true,
@@ -36,6 +38,20 @@ describe('apiCalls', () => {
       global.fetch = mockFetch;
 
       await expect(fetchSynonyms('happy')).resolves.toEqual(['results'])
+    })
+
+    it('should return an error if response not ok (SAD)', async () => {
+      let mockFetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        })
+      })
+
+      let expectedError = Error('There was an error when fetching your synonyms. Please try again.')
+
+      global.fetch = mockFetch;
+
+      await expect(fetchSynonyms('happy')).resolves.toEqual(expectedError)
     })
   })
 })
