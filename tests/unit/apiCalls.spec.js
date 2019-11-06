7c17e45ea8098cc4b3d01659eb1620a5672dc211
Brianna DelValle <briannadelvalle@gmail.com>
2019-11-06 09:37:57 -0700

    Write happy path test for fetchSynonyms

--- tests/unit/apiCalls.spec.js
+++ tests/unit/apiCalls.spec.js
@@ -2,15 +2,40 @@ import { fetchSynonyms } from '../../src/api/apiCalls.js';
 
 describe('apiCalls', () => {
   describe('fetchSynonyms', () => {
-    it('should fire fetch with the correct url', () => {
+    it('should fire fetch with the correct url', async () => {
       let expectedUrl = `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/happy?key=${process.env.VUE_APP_API_KEY}`
 
-      let mockFetch = jest.fn()
+      let mockFetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => {
+            return mockResults
+          }
+        })
+      })
       global.fetch = mockFetch
 
-      fetchSynonyms('happy');
+      await fetchSynonyms('happy');
 
       expect(mockFetch).toHaveBeenCalledWith(expectedUrl)
+    })
+
+    it('should return the first index of array of results when successful (HAPPY)', async () => {
+
+      let mockResults = [['results'], ['more results']]
+
+      let mockFetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => {
+            return mockResults
+          }
+        })
+      })
+
+      global.fetch = mockFetch;
+
+      await expect(fetchSynonyms('happy')).resolves.toEqual(['results'])
     })
   })
 })
